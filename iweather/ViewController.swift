902e8a90f8bde6b61ecd9658d167f20d3818ffed
Anders Ullnæss <andersu@Mac-L-Moore.local>
2014-10-20 21:57:05 +0200

    3b

--- iweather/ViewController.swift
+++ iweather/ViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var nameLabel: UILabel!
     
@@ -30,6 +30,24 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    //Oppgave 3b
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // TODO: implementer denne
+        return 0
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // Hint: Du trenger noe som dette og må returnere cellen du lager
+        // var cell = self.weatherTable.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        // Oppgave 3b) 4) fylle ut tekst i celle
+        
+        return UITableViewCell()
     }
 }
 
